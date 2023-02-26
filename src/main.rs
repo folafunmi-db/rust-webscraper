17a141fa51649cefb9ba8a5767a806f4ef46ad16
Folafunmi <folafunmimustapha@gmail.com>
2023-02-26 22:13:32 +0100

    feat: fetch site content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let response = reqwest::blocking::get(
+        "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100",
+    )
+    .unwrap()
+    .text()
+    .unwrap();
+
+    print!("{response}");
 }
