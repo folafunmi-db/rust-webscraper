5cbac2b248afbf1118727ebf7156bf1cf734fda6
Folafunmi <folafunmimustapha@gmail.com>
2023-03-05 23:44:15 +0100

    feat: complete weg scraper

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use std::env;
+
 fn main() {
+    // let args: Vec<String> = env::args().collect();
+
+    // let url = &args[1];
+    // let selector = &args[2];
+
+    // println!("url: {}, selector: {}", url, selector);
+
     let response = reqwest::blocking::get(
         "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100",
     )
@@ -6,5 +15,13 @@ fn main() {
     .text()
     .unwrap();
 
-    print!("{response}");
+    let document = scraper::Html::parse_document(&response);
+
+    let title_selector = scraper::Selector::parse("h3.lister-item-header>a").unwrap();
+
+    let titles = document.select(&title_selector).map(|x| x.inner_html());
+
+    titles
+        .zip(1..101)
+        .for_each(|(item, number)| println!("{}, {}", number, item));
 }
